554a6b022005a6634a7295fc6e30fa004dcdff7d
CristhianJR4 <c>
2022-12-01 18:38:12 -0800

    View Product

--- Views/ProductView.swift
+++ Views/ProductView.swift
@@ -11,41 +11,53 @@ import FirebaseAuth
 struct ProductView: View {
     @ObservedObject var productVM = ProductVM()
     var body: some View {
-        
-        
-        
-            List(productVM.products){ product in
+        HStack{
+            Text("Shop")
+                .padding(7)
+                .bold()
+            Spacer()
+            Button(action: {
                 
-                HStack{
+            }, label: {
+                Image(systemName: "cart")
+                    .padding(7)
+                    .foregroundColor(.black)
+            })
+            
+        }
+                List(productVM.products){ product in
                     
-                    FirebaseImage(id: product.image)
-                        .frame(width: 5,height: 2)
-                        .padding(.horizontal,30)
-                    Spacer()
-                    
-                    VStack{
+                    HStack{
+                        
+                        FirebaseImage(id: product.image)
+                            .frame(width: 5,height: 2)
+                            .padding(.horizontal,30)
+                        Spacer()
+                        
+                        VStack{
                             Text(product.name).font(.title)
                             Text(product.price).font(.caption)
+                            
+                        }
+                        .padding()
                         
+                        Button(action: {
+                            
+                        }, label: {
+                            Image(systemName: "plus")
+                                .padding(10)
+                                .foregroundColor(.white)
+                                .background(.black)
+                                .cornerRadius(50)
+                        })
                     }
-                    .padding()
-                    
-                    Button(action: {
-                        
-                    }, label: {
-                        Image(systemName: "plus")
-                            .padding(10)
-                            .foregroundColor(.white)
-                            .background(.black)
-                            .cornerRadius(50)
-                    })
                 }
-            }
-            .onAppear(){
-                self.productVM.getData()
-                
-            }
-        
+                .onAppear(){
+                    self.productVM.getData()
+                    
+                }
+         
+     
         
         Button(action: {
                 self.logout()
